6176d6bebf9d431c306870e5cf4278fbe5f0d78d
Michel Schudel <michel.schudel@gmail.com>
2017-03-09 11:05:51 +0100

    add extra commands

--- src/main/java/org/cyanotic/cx10/controllers/HackatonController.java
+++ src/main/java/org/cyanotic/cx10/controllers/HackatonController.java
@@ -1,4 +1,7 @@
 package org.cyanotic.cx10.controllers;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.cyanotic.cx10.api.Command;
 import org.cyanotic.cx10.api.Controller;
 
@@ -7,35 +10,47 @@ public class HackatonController implements Controller {
     private static final Command TAKEOFF_COMMAND = new Command(0, 0, 0, 0, true, false);
     private static final Command LAND_COMMAND = new Command(0, 0, 0, 0, false, true);
     private static final Command IDLE_COMMAND = new Command(0, 0, 0, 0, false, false);
-    private static final Command TURN_RIGHT_COMMAND = new Command(0, 64, 0, 0, false, false);
+    private static final Command TURN_RIGHT_COMMAND = new Command(0, 127, 0, 0, false, false);
     private static final Command TURN_LEFT_COMMAND = new Command(0, -64, 0, 0, false, false);
-    private static final Command FORWARD_COMMAND = new Command(64, 0, 0, 64, false, false);
-    private static final Command BACKWARD_COMMAND = new Command(-64, 0, 0, 64, false, false);
+    private static final Command FORWARD_COMMAND = new Command(64, 0, 0, 0, false, false);
+    private static final Command BACKWARD_COMMAND = new Command(-64, 0, 0, 0, false, false);
 
     private boolean takenOff = false;
 
     private int teller = 0;
 
-    @Override public Command getCommand() {
+    private static int OFFSET = 150;
+
+    private Command currentCommand = TAKEOFF_COMMAND;
+
+    private static Map<Integer, Command> scenario = new HashMap<>();
+
+    static {
+        scenario.put(1, IDLE_COMMAND);
+        scenario.put(50, TURN_RIGHT_COMMAND);
+        scenario.put(75, TURN_LEFT_COMMAND);
+        scenario.put(100, FORWARD_COMMAND);
+        scenario.put(125, BACKWARD_COMMAND);
+        scenario.put(150, LAND_COMMAND);
+    }
+    @Override
+    public Command getCommand() {
+        int nettoTeller = getNettoTeller();
         teller++;
-        System.out.println("teller:" + teller);
-        if (!takenOff) {
-            takenOff = true;
-            System.out.println("taking off");
-            return TAKEOFF_COMMAND;
-        } else if (teller < 50 || (teller > 60 && teller < 100) ){
-            System.out.println("idle");
-            return IDLE_COMMAND;
-        } else if (teller >= 50 && teller <= 60) {
-            System.out.println("turning right");
-            return TURN_RIGHT_COMMAND;
-        } else {
-            System.out.println("landing");
-            return LAND_COMMAND;
+        if (scenario.containsKey(nettoTeller)) {
+            Command command = scenario.get(nettoTeller);
+            System.out.println("altering command " + command.toString() + " on teller " + getNettoTeller());
+            currentCommand = command;
         }
+        return currentCommand;
     }
 
-    @Override public void close() {
-        //do nothing
+    private int getNettoTeller() {
+        return teller - OFFSET;
+    }
+
+    @Override
+    public void close() {
+        // do nothing
     }
 }
